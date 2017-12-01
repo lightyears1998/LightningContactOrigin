09aece7268222c3a8f5bc9bf76c61c7544b32d49
lightyears1998 <lightyears1998@hotmail.com>
2017-12-01 10:07:47 +0800

    重定义常量表

--- strings.h
+++ strings.h
@@ -1,18 +1,18 @@
-static const char * PROJECT_NAME = "联系人管理系统";
-static const char * SLASH        = " - ";
-static const char * BANNER       = "================================";
+#define PROJECT_NAME        "联系人管理系统"
+#define SLASH               " - "
+#define BANNER              "================================"
 
-static const char * NEW_LINE     = "\n";
-static const char * INPUT_HINT   = ">> ";
+#define NEW_LINE            "\n"
+#define INPUT_HINT          ">> "
 
-static const char * MODULE_MAIN_MENU = "主菜单";
-static const char * MODULE_ADD       = "添加记录";
-static const char * MODULE_SHOW      = "浏览记录";
-static const char * MODULE_DELETE    = "删除记录";
-static const char * MODULE_QUERY     = "查询记录";
-static const char * MODULE_MODIFY    = "修改记录";
-static const char * MODULE_SAVE      = "保存记录";
-static const char * MODULE_SORT      = "排序记录";
-static const char * MODULE_EXIT      = "退出程序";
+#define MODULE_MAIN_MENU    "主菜单"
+#define MODULE_ADD          "添加记录"
+#define MODULE_SHOW         "浏览记录"
+#define MODULE_DELETE       "删除记录"
+#define MODULE_QUERY        "查询记录"
+#define MODULE_MODIFY       "修改记录"
+#define MODULE_SAVE         "保存记录"
+#define MODULE_SORT         "排序记录"
+#define MODULE_EXIT         "退出程序"
 
-static const char * GOODBYE      = "再见 : )";
+#define GOODBYE             "再见 : )"
